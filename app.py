f94a55269c238cd9d5b34aaa6e39ab579151dc67
prabodhw96 <prabodhwankhede1996@gmail.com>
2021-02-18 01:22:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import awesome_streamlit as ast
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+import plotly.express as px
+
+import villages
+import stats
+import find
+
+ast.core.services.other.set_logging_format()
+
+PAGES = {
+	"Villages": villages,
+	"Statistics": stats,
+	"Find Person": find
+}
+
+def main():
+	st.sidebar.title("Navigation")
+	selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+	page = PAGES[selection]
+
+	with st.spinner(f"Loading {selection}..."):
+		ast.shared.components.write_page(page)
+
+if __name__ == "__main__":
+	main()
